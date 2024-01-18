d30b14774e55a4ad37c154f9a5ccbf8aff83262e
aditya711-code <adityamane711@gmail.com>
2024-01-18 17:00:11 +0530

    Add mongodb-config

--- index.js
+++ index.js
@@ -3,15 +3,19 @@ import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
 import authRoutes from './routes/auth.js';
+import mongoose from 'mongoose';
+
 dotenv.config()
 const app=express();
 app.use(express.json())
 app.use(bodyParser.json({ limit: "50mb", extended: true }));
 app.use(morgan("common"))
-const PORT =  6001;
-app.listen(PORT,()=>{
-    console.log(`Server running on ${PORT}`)
-
-})
+const PORT =  process.env.PORT;
 app.use('/auth',authRoutes);
-
+mongoose.connect(process.env.MONGO_URL).then(()=>{
+    app.listen(PORT,"0.0.0.0",()=>{
+        console.log(`Server running on: ${PORT}`);
+    })
+}).catch((error)=>{
+    console.log(`${error} did not connect`)
+})
