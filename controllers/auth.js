3a545e80f1aebaef9d2fdebf93867e85770c2b89
aditya711-code <adityamane711@gmail.com>
2024-01-18 17:51:15 +0530

    Add login feature

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,11 @@
 import bcrypt from "bcrypt";
 import User from '../models/User.js'
+import jwt from "jsonwebtoken";
+
 /*Register User*/
 export const register = async (req, res) => {
   try {
     const { firstName, lastName, email, password } = req.body;
-    console.log("req",firstName,lastName)
     const salt = await bcrypt.genSalt();
     const passwordHash = await bcrypt.hash(password, salt);
     const newUser = new User({
@@ -20,3 +21,22 @@ export const register = async (req, res) => {
   }
 };
 
+/*Login user*/
+export const login=async(req,res)=>{
+  try{
+      const{email,password}=req.body;
+      const user=await User.findOne({email:email});
+      if(!user){
+        return res.status(400).json({message:"User does not exist"});
+      }
+      const isMatch=await bcrypt.compare(password,user.password)
+      if(!isMatch)
+        return res.status(400).json({message:"Invalid credentials"});
+
+      const token=jwt.sign({id:user._id},process.env.JWT_SECRET);
+      delete user.password;
+      res.status(200).json({token,user});
+  }catch(error){
+      res.status(500).json(error);
+  }
+}
