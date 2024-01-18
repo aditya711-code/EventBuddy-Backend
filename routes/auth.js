06439b4078394872218ce7481d311cdd8deba334
aditya711-code <adityamane711@gmail.com>
2024-01-18 15:23:11 +0530

    Delete comment

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,5 @@
 import express from 'express'
 import {register} from '../controllers/auth.js'
 const router = express.Router();
-
-console.log("register user")
 router.post("/register", register);
 export default router;  
